183fb787c854d5b4ce01178727a17b6e4ee14211
Gregory Daynes <greg@stembolt.com>
2017-02-23 13:30:21 -0800

    Add Presentation Model
    The `Presentation` model is pretty straight forward. Setting up the
    associations to `Event` and `Person`.
    
    Validations for `Presentation` are also pretty relaxed. Be aware that
    `presenter` and `person` are conditionally dependent on eachother. If
    one is set, the other cannot be.
    
    `Presentation` adds a new property of `presenter_name` which will return
    either the `presenter` if present, or the association
    `Person`.`first_name`
    
    The Presentation Factory includes associations for `Person` and `Event`
    for ease of testing. If you need to test `presenter` you will need to
    unset person or the model will be invalid.

--- spec/models/presentation_spec.rb
+++ spec/models/presentation_spec.rb
@@ -1,5 +1,97 @@
+# spec/models/presentation_spec.rb
 require 'rails_helper'
 
 RSpec.describe Presentation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:person) { FactoryGirl.create(:person) }
+  let(:event)  { FactoryGirl.create(:event) }
+
+  describe 'validate model' do
+    subject { FactoryGirl.create(:presentation) }
+
+    it { is_expected.to validate_presence_of :topic }
+    it { is_expected.to validate_presence_of :duration }
+
+    it { is_expected.to belong_to :person }
+    it { is_expected.to belong_to :event }
+
+    it { is_expected.to be_valid }
+
+    describe 'person and presenter validation' do
+      subject { described_class.new(presentation) }
+
+      context 'with presenter, no person' do
+        let(:presentation) do
+          FactoryGirl.attributes_for(
+            :presentation,
+            event_id: event.id,
+            presenter: 'test'
+          )
+        end
+
+        it { is_expected.to be_valid }
+        it 'is expected to have person: nil, presenter: "test"' do
+          expect(subject).to have_attributes(
+            person: nil,
+            presenter: presentation[:presenter]
+          )
+        end
+      end
+
+      context 'with person, no presenter' do
+        let(:presentation) do
+          FactoryGirl.attributes_for(
+            :presentation,
+            event_id: event.id,
+            person_id: person.id
+          )
+        end
+
+        it { is_expected.to have_attributes(person: person, presenter: nil) }
+        it { is_expected.to be_valid }
+      end
+
+      context 'with presenter, and person' do
+        let(:presentation) do
+          FactoryGirl.attributes_for(
+            :presentation,
+            event_id: event.id,
+            presenter: 'test',
+            person_id: person.id
+          )
+        end
+
+        it { is_expected.to_not be_valid }
+        it 'is expected to have person and presenter' do
+          expect(subject).to have_attributes(
+            person: person,
+            presenter: presentation[:presenter]
+          )
+        end
+      end
+
+      context 'without presenter, or person' do
+        let(:presentation) do
+          FactoryGirl.attributes_for(:presentation, event_id: event.id)
+        end
+
+        it { is_expected.to_not be_valid }
+      end
+    end
+  end
+
+  describe 'presenter_name' do
+    context 'when presenter is not present' do
+      subject { FactoryGirl.create(:presentation) }
+
+      it { is_expected.to have_attributes(presenter_name: 'John') }
+    end
+
+    context 'when presenter is present' do
+      subject do
+        FactoryGirl.create(:presentation, presenter: 'Testy', person: nil)
+      end
+
+      it { is_expected.to have_attributes(presenter_name: 'Testy') }
+    end
+  end
 end
