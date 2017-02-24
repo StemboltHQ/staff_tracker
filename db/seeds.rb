edea8123a895847738b71d221a64c30d1e7c6670
Gregory Daynes <greg@stembolt.com>
2017-02-24 11:36:57 -0800

    Add Seed Data
    Seed data for interacting with the application on development.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Person Seeder
+5.times do |i|
+  Person.create(
+    first_name: 'Person',
+    last_name: "#{i}",
+    email: "person.#{i}@example.com",
+    date_of_birth: Date.new(1970,1,1),
+    password: 'abc123',
+    password_confirmation: 'abc123',
+    gender: 'Seed'
+  )
+end
+
+# Event Seeder
+Event.create(
+  date: Date.new(2017, 3, 1),
+  location: 'Stembolt Offices',
+  name: 'Show & Tell'
+)
+Event.create(
+  date: Date.new(2027, 1, 1),
+  location: 'Stembolt - Mars Office',
+  name: 'Show & Tell'
+)
+
+# Presentation Seeder
+3.times do |i|
+  Presentation.create(
+    topic: "Presentation #{i}",
+    description: 'Super awesome presentation',
+    duration: 900,
+    person_id: Person.pluck(:id).sample,
+    event_id: Event.pluck(:id).sample
+  )
+end
+
+6.times do |i|
+  Presentation.create(
+    topic: "Presentation #{i}",
+    description: 'Super awesome presentation',
+    duration: 900,
+    presenter: "Guest #{i}",
+    event_id: Event.pluck(:id).sample
+  )
+end
