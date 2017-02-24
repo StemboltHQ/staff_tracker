66bf11725d1b1dce98354478e0f2e49a7ec8b170
Gregory Daynes <greg@stembolt.com>
2017-02-24 08:38:52 -0800

    Update Person Model + Tests
    Found some failures in the model, false positives on the validations.
    Clean them up, and refactor.
    
    Cleanup tests and make use of FactoryGirl and shoulda-matchers.

--- app/models/person.rb
+++ app/models/person.rb
@@ -6,31 +6,28 @@ class Person < ApplicationRecord
     gender.downcase!
   end
 
-  valid_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  valid_email_regex = /.+@.+\..+/i
 
+  validates :first_name, length: { minimum: 2 }
   validates :first_name,
             presence: true,
-            length: { minimum: 2 },
-            if: -> { new_record? || !first_name.nil? }
+            if: -> { new_record? || first_name.blank? }
+
   validates :email,
             presence: true,
-            format: { with: valid_email_regex },
-            uniqueness: { case_sensitive: false },
-            if: -> { new_record? || !email.nil? }
+            if: -> { new_record? || email.blank? }
+  validates :email,
+            format: { with: valid_email_regex }
+  validates :email,
+            uniqueness: { case_sensitive: false }
+
   validates :date_of_birth,
             presence: true,
-            if: -> { new_record? || !date_of_birth.nil? }
+            if: -> { new_record? || date_of_birth.blank? }
+
   validates :gender,
             presence: true,
-            if: -> { new_record? || !gender.nil? }
-  validates :password,
-            presence: true,
-            length: { minimum: 6 },
-            if: -> { new_record? || !password.nil? }
-  validates :password_confirmation,
-            presence: true,
-            length: { minimum: 6 },
-            if: -> { new_record? || !password_confirmation.nil? }
+            if: -> { new_record? || gender.blank? }
 
   has_secure_password
 end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,82 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Person, type: :model do
-  subject do
-    described_class.new(
-      first_name: 'Gregory',
-      last_name: 'Daynes',
-      email: 'greg@stembolt.com',
-      password: 'abc123',
-      password_confirmation: 'abc123',
-      date_of_birth: Date.new(1986, 11, 4),
-      gender: 'Male'
-    )
-  end
+  subject { FactoryGirl.create(:person) }
 
-  describe "validations" do
-    it "expects valid attributes" do
-      expect(subject).to be_valid
-    end
-    it "expects first_name" do
-      subject.first_name = nil
-      expect(subject).to_not be_valid
-    end
+  describe 'validations' do
+    it { is_expected.to validate_presence_of :first_name }
+    it { is_expected.to validate_presence_of :email }
+    it { is_expected.to validate_presence_of :date_of_birth }
+    it { is_expected.to validate_presence_of :gender }
+    it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+    it { is_expected.to have_secure_password }
+    it { is_expected.to be_valid }
+
     it "expects first_name to be longer than 1 character" do
       subject.first_name = "A"
       expect(subject).to_not be_valid
     end
-    it "expects email" do
-      subject.email = nil
-      expect(subject).to_not be_valid
-    end
-    it "expects valid email" do
-      subject.email = "greg@stembolt"
-      expect(subject).to_not be_valid
-    end
-    it "expects unique email" do
-      duplicate_person = subject.dup
-      subject.save
-      expect(duplicate_person).to_not be_valid
-    end
+
     it "expects emails to be stored lower-case" do
       subject.email = "GreG@StemBolt.CoM"
       subject.save
       expect(subject.email).to eq(subject.email.downcase)
     end
-    it "expects date_of_birth" do
-      subject.date_of_birth = nil
-      expect(subject).to_not be_valid
-    end
-    it "accepts date_of_birth as Date" do
-      expect(subject).to be_valid
-    end
-    it "accepts date_of_birth as String" do
-      subject.date_of_birth = '1986/11/04'
-      expect(subject).to be_valid
-    end
-    it "expects gender" do
-      subject.gender = nil
-      expect(subject).to_not be_valid
-    end
+
     it "expects gender to be stored lower-case" do
       subject.save
       expect(subject.gender).to eq(subject.gender.downcase)
     end
-    it "expects password" do
-      subject.password = nil
-      expect(subject).to_not be_valid
-    end
-    it "expects password_confirmation" do
-      subject.password_confirmation = nil
-      expect(subject).to_not be_valid
-    end
-    it "expects password == password_confirmation" do
-      subject.password_confirmation = "123abc"
-      expect(subject).to_not be_valid
-    end
-  end
-
-  describe "associations" do
-    it "should have one staff"
   end
 end
