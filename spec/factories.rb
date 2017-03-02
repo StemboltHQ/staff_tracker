cc12521e98abcedff55519e89ed0b28ece41536c
dennis <dennis@freerunningtechnologies.com>
2017-03-02 09:16:02 -0800

    Add event model requiring notification query
    The application will be notifying presenters of their upcoming
    presentations. Because of this we need to have a query on the event
    model that returns all the events that require notification.

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,11 +3,25 @@ FactoryGirl.define do
   factory :person do
     first_name 'John'
     last_name 'Doe'
-    email "john.doe#{Random.rand(1000)}@example.com"
+    email { "john.doe#{Random.rand(1000)}@example.com" }
     password 'abc123'
     password_confirmation 'abc123'
     date_of_birth Date.new(1970, 1, 1)
     gender 'Computer'
+  end
+
+  factory :event do
+    name 'Show & Tell'
+    location 'Board Room'
+    date Date.new(2017, 2, 24)
+
+    trait :requiring_notification do
+      date Time.zone.today - Event::NOTIFICATION_PERIOD
+    end
+
+    trait :not_requiring_notification do
+      date Time.zone.today + Event::NOTIFICATION_PERIOD
+    end
   end
 
   factory :presentation do
@@ -16,11 +30,9 @@ FactoryGirl.define do
     duration 900
     person
     event
-  end
 
-  factory :event do
-    name 'Show & Tell'
-    location 'Board Room'
-    date Date.new(2017, 2, 24)
+    trait :requiring_notification do
+      event { FactoryGirl.build(:event, :requiring_notification) }
+    end
   end
 end
