84dae21869f4f4b4cee1a34a3276b95ea8d47c04
dennis <dennis@freerunningtechnologies.com>
2017-03-15 15:18:52 -0700

    Apply pundit to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 class ApplicationController < ActionController::Base
+  include Pundit
   protect_from_forgery with: :exception
   helper_method :current_person
 
   def current_person
     @current_person ||= Person.find(session[:person_id]) if session[:person_id]
   end
+
+  alias_method :current_user, :current_person
 end
