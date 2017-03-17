f35c6d31fcd32d64caa6d31b766db333aec8eaa4
dennis <dennis@freerunningtechnologies.com>
2017-03-17 11:33:13 -0700

    Create welcome controller spec

---
+++ spec/controllers/welcome_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe WelcomeController, type: :controller do
+  describe 'GET #index' do
+    subject { get :index }
+
+    it { is_expected.to be_successful }
+    it { is_expected.to render_template(:index) }
+  end
+end
