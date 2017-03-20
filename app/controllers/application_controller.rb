e0e71c1d923898d1fb38c158e7d9e96193c36964
dennis <dennis@freerunningtechnologies.com>
2017-03-20 08:57:21 -0700

    Updated application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,17 @@ class ApplicationController < ActionController::Base
   helper_method :current_person
   helper_method :authenticate_person
 
+  private
+
   def current_person
     @current_person ||= Person.find(session[:person_id]) if session[:person_id]
   end
 
   def authenticate_person
-    redirect_to sign_in_path unless current_person
+    unless current_person
+      redirect_to sign_in_path
+      return false
+    end
   end
 
   alias_method :current_user, :current_person
