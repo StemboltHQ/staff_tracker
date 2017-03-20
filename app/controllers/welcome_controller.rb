ea473dda18a304393f7c226cde844338f95591af
dennis <dennis@freerunningtechnologies.com>
2017-03-20 09:32:31 -0700

    sign in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,6 @@ class WelcomeController < ApplicationController
   def index
     @next_event = Event.upcoming.order(:date).first
   end
+
+  def sign_in; end
 end
--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe WelcomeController, type: :controller do
     it { is_expected.to be_successful }
     it { is_expected.to render_template(:index) }
   end
+
+  describe 'GET #sign_in' do
+    subject { get :sign_in }
+
+    it { is_expected.to be_successful }
+    it { is_expected.to render_template(:sign_in) }
+  end
 end
