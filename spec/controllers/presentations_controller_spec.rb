2b563db7d4ce18c498a7df960c20ccd9cb8b0520
dennis <dennis@freerunningtechnologies.com>
2017-03-23 10:53:13 -0700

    Update presentations controller
    Added a index action to the presentation controller

--- spec/controllers/presentations_controller_spec.rb
+++ spec/controllers/presentations_controller_spec.rb
@@ -82,5 +82,12 @@ RSpec.describe PresentationsController, type: :controller do
         end
       end
     end
+
+    describe 'GET #index' do
+      subject { get :index }
+
+      it { is_expected.to render_template :index }
+      it { is_expected.to be_successful }
+    end
   end
 end
