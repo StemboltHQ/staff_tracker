cd9498a2d6f5a3329a9d5484627caa239eebe92b
Nat York <natalie@freerunningtechnologies.com>
2017-03-23 15:04:13 -0700

    Refactor spec for presentation controller
    Reordered specs such that the describe block for each method is at the
    highest level within the spec and contexts, including logged-in status
    and user role are nested below that. Also added specs for:
    
    GET #Edit
    PATCH #Update
    DELETE #Destroy

--- spec/controllers/presentations_controller_spec.rb
+++ spec/controllers/presentations_controller_spec.rb
@@ -1,28 +1,122 @@
 require 'rails_helper'
 
 RSpec.describe PresentationsController, type: :controller do
-  context 'the current person is an admin' do
-    let(:admin_person) { FactoryGirl.build(:person, :admin) }
+  let(:nil_person) { nil }
+  let(:non_admin) { FactoryGirl.build(:person) }
+  let(:admin_person) { FactoryGirl.build(:person, :admin) }
 
-    before { mock_pundit_user_as(admin_person) }
+  describe 'GET #index' do
+    subject { get :index }
 
-    describe 'GET #new' do
-      subject { get :new }
+    context 'the current person is not signed in' do
+      before { mock_pundit_user_as(nil_person) }
+
+      it { is_expected.to render_template :index }
+      it { is_expected.to be_successful }
+    end
+  end
+
+  describe 'GET #show' do
+    subject { get :show, params: { id: presentation.id } }
+    let!(:presentation) { FactoryGirl.create(:presentation) }
+
+    context 'the current person is not signed in' do
+      before { mock_pundit_user_as(nil_person) }
+
+      it { is_expected.to render_template :show }
+      it { is_expected.to be_successful }
+    end
+  end
+
+  describe 'GET #new' do
+    subject { get :new }
+
+    context 'the current person is not signed in' do
+      before { mock_pundit_user_as(nil_person) }
+
+      it 'throws a not authorized error' do
+        expect { subject }.to raise_error(Pundit::NotAuthorizedError)
+      end
+    end
+
+    context 'the current person is a non-admin' do
+      before { mock_pundit_user_as(non_admin) }
 
       it { is_expected.to render_template :new }
       it { is_expected.to be_successful }
     end
+  end
 
-    describe 'POST #create' do
-      subject { post :create, params: { presentation: params } }
-      let!(:person) { FactoryGirl.create(:person) }
-      let!(:event) { FactoryGirl.create(:event) }
+  describe 'GET #edit' do
+    subject { get :edit, params: { id: presentation.id } }
+    let(:presentation) { FactoryGirl.create(:presentation) }
+
+    context 'the current person is a non-admin' do
+      before { mock_pundit_user_as(non_admin) }
+
+      it 'throws a not authorized error' do
+        expect { subject }.to raise_error(Pundit::NotAuthorizedError)
+      end
+    end
+
+    context 'the current person is an admin' do
+      before { mock_pundit_user_as(admin_person) }
+
+      it { is_expected.to render_template :edit }
+      it { is_expected.to be_successful }
+    end
+  end
+
+  describe 'POST #create' do
+    subject { post :create, params: { presentation: params } }
+    let(:event) { FactoryGirl.create(:event) }
+    let(:person) { FactoryGirl.create(:person) }
+
+    context 'the current person is not signed in' do
+      before { mock_pundit_user_as(nil_person) }
+
+      let(:params) do
+        FactoryGirl.attributes_for(
+          :presentation,
+          event_id: event.id
+        )
+      end
+
+      it 'throws a not authorized error' do
+        expect { subject }.to raise_error(Pundit::NotAuthorizedError)
+      end
+    end
+
+    context 'the current person is a non-admin' do
+      before { mock_pundit_user_as(non_admin) }
+
+      let(:params) do
+        FactoryGirl.attributes_for(
+          :presentation,
+          event_id: event.id
+        )
+      end
+
+      it { is_expected.to redirect_to Presentation.last }
+
+      it 'is expected to be successful' do
+        expect(response).to be_success
+      end
+
+      it 'is expected to change presentation count +1' do
+        expect { subject }.to change(Presentation, :count).by(1)
+      end
+    end
+
+    context 'the current person is an admin' do
+      before { mock_pundit_user_as(admin_person) }
 
       context 'valid params' do
         let(:params) do
           FactoryGirl.attributes_for(
             :presentation,
-            event_id: event.id
+            event_id: event.id,
+            person_id: person.id
           )
         end
 
@@ -49,45 +143,72 @@ RSpec.describe PresentationsController, type: :controller do
       end
     end
   end
-  context 'the current person is a non-admin' do
-    let(:non_admin) { FactoryGirl.build(:person) }
 
-    before { mock_pundit_user_as(non_admin) }
-
-    describe 'GET #show' do
-      subject { get :show, params: { id: presentation.id } }
-      let!(:presentation) { FactoryGirl.create(:presentation) }
-
-      it { is_expected.to render_template :show }
-      it { is_expected.to be_successful }
+  describe 'PATCH #update' do
+    subject do
+      patch :update, params: { id: presentation.id, presentation: attr }
     end
-  end
+    let(:presentation) { FactoryGirl.create(:presentation) }
 
-  context 'the current person is not signed in' do
-    let(:nil_person) { nil }
+    context 'the current person is a non-admin' do
+      before { mock_pundit_user_as(non_admin) }
+      let(:attr) { { topic: 'New Topic' } }
 
-    before { mock_pundit_user_as(nil_person) }
-
-    describe 'POST #create' do
-      subject { post :create, params: { presentation: params } }
-      let(:event) { FactoryGirl.create(:event) }
-      let(:params) do
-        FactoryGirl.attributes_for(
-          :presentation,
-          event_id: event.id
-        )
-
-        it 'throws a not authorized error' do
-          expect { subject }.to raise_error(Pundit::NotAuthorizedError)
-        end
+      it 'throws a not authorized error' do
+        expect { subject }.to raise_error(Pundit::NotAuthorizedError)
       end
     end
 
-    describe 'GET #index' do
-      subject { get :index }
+    context 'the current person is an admin' do
+      before { mock_pundit_user_as(admin_person) }
 
-      it { is_expected.to render_template :index }
-      it { is_expected.to be_successful }
+      context 'changing topic' do
+        let(:attr) { { topic: 'New Topic' } }
+
+        it { is_expected.to redirect_to presentation }
+
+        it 'should update presentation' do
+          expect { subject }
+            .to change { presentation.reload.topic }
+            .from('Being Awesome')
+            .to('New Topic')
+        end
+      end
+
+      context 'invalid attributes' do
+        let(:attr) { { topic: nil } }
+
+        it { is_expected.to render_template :edit }
+
+        it 'should not update presentation' do
+          expect { subject }
+            .to_not change { presentation.reload.topic }
+            .from('Being Awesome')
+        end
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    subject { delete :destroy, params: { id: presentation.id } }
+    let!(:presentation) { FactoryGirl.create(:presentation) }
+
+    context 'the current person is a non-admin' do
+      before { mock_pundit_user_as(non_admin) }
+
+      it 'throws a not authorized error' do
+        expect { subject }.to raise_error(Pundit::NotAuthorizedError)
+      end
+    end
+
+    context 'the current person is an admin' do
+      before { mock_pundit_user_as(admin_person) }
+
+      it { is_expected.to redirect_to presentations_path }
+
+      it 'changes presentation count by -1' do
+        expect { subject }.to change(Presentation, :count).by(-1)
+      end
     end
   end
 end
