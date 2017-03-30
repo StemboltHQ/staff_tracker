567296066069d01ac7890a6e5cd9b0760d23fcc5
dennis <dennis@freerunningtechnologies.com>
2017-03-30 09:44:07 -0700

    Add create action to topic requests controller

--- spec/controllers/topic_requests_controller_spec.rb
+++ spec/controllers/topic_requests_controller_spec.rb
@@ -7,4 +7,29 @@ RSpec.describe TopicRequestsController, type: :controller do
     it { is_expected.to render_template :index }
     it { is_expected.to be_successful }
   end
+
+  describe 'POST #create' do
+    subject { post :create, params: { topic_request: params } }
+
+    context 'with valid attributes' do
+      let(:params) { FactoryGirl.attributes_for(:topic_request) }
+
+      it 'redirect to presentations/requests' do
+        expect(subject).to redirect_to(requests_presentations_path)
+      end
+      it 'changes the topic request count +1' do
+        expect { subject }.to change(TopicRequest, :count).by(1)
+      end
+    end
+
+    context 'with invalid attributes' do
+      let(:params) { FactoryGirl.attributes_for(:topic_request, name: nil) }
+
+      it { is_expected.to be_successful }
+      it { is_expected.to render_template :index }
+      it 'does not change the topic request count' do
+        expect { subject }.to_not change(TopicRequest, :count)
+      end
+    end
+  end
 end
