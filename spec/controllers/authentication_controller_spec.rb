0503a49079fce751bec969094b74b7b9fd1d5467
Nat York <natalie@freerunningtechnologies.com>
2017-04-04 13:54:56 -0700

    Update authentication controller redirect

--- spec/controllers/authentication_controller_spec.rb
+++ spec/controllers/authentication_controller_spec.rb
@@ -10,14 +10,33 @@ RSpec.describe AuthenticationController, type: :controller do
       post :google
       response
     end
+
     it 'creates a new person' do
       expect { subject }.to change { Person.count }.by(1)
     end
-    it 'redirects the person to the root url' do
-      expect(subject).to redirect_to(root_url)
-    end
+
     it 'creates a new session' do
       expect { subject }.to change { session[:person_id] }.from nil
+    end
+
+    context 'omniauth origin is nil' do
+      before do
+        request.env['omniauth.origin'] = nil
+      end
+
+      it 'redirects the person to the root url' do
+        expect(subject).to redirect_to(root_url)
+      end
+    end
+
+    context 'omniauth origin is not nil' do
+      before do
+        request.env['omniauth.origin'] = 'http://test.host/presentations'
+      end
+
+      it 'redirects the person to the path location saved prior to login' do
+        expect(subject).to redirect_to(presentations_path)
+      end
     end
   end
 end
