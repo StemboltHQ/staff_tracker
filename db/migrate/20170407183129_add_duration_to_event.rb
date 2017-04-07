fda2fff22554e1cd27342d1a17a3126fb9e271b8
dennis <dennis@freerunningtechnologies.com>
2017-04-07 11:34:09 -0700

    migration

---
+++ db/migrate/20170407183129_add_duration_to_event.rb
@@ -0,0 +1,4 @@
+class AddDurationToEvent < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170406154040) do
+ActiveRecord::Schema.define(version: 20170407183129) do
 
   create_table "events", force: :cascade do |t|
     t.date     "date",       null: false
